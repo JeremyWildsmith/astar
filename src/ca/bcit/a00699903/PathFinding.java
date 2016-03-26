a33035a96a3309214578f94b7fc276f59b5b031f
Jeremy Wildsmith <jeremywildsmith@yahoo.ca>
2016-03-26 04:10:56 -0700

    Code clean-up, fixed bug when printing out f-value map.

--- src/ca/bcit/a00699903/PathFinding.java
+++ src/ca/bcit/a00699903/PathFinding.java
@@ -17,6 +17,8 @@ public class PathFinding {
         Vector start = new Vector(0, 0);
         Vector end = new Vector(3, 3);
         
+        System.out.println("Map below. s=start location; d=destination");
+        
         for(int y = 0; y < map.length; y++)
         {
             for(int x = 0; x < map[y].length; x++)
@@ -32,6 +34,19 @@ public class PathFinding {
             System.out.println();
         }
         
+        System.out.println("\nF Value score map");
+        
+        for(int y = 0; y < 5; y++)
+        {
+            for(int x = 0; x < 5; x++)
+            {
+                int score = new Node(new Vector(x,y)).getF(end);
+                System.out.print("[" + score + "]");
+            }
+            System.out.println();
+        }
+        
+        System.out.println("\nDirections to reach destination: ");
         Node path = pathFind(start, end, map);
 
         Direction[] directions = path.getDirections();
@@ -42,15 +57,6 @@ public class PathFinding {
             System.out.println(d);
         }
         
-        for(int y = 0; y < 5; y++)
-        {
-            for(int x = 0; x < 5; x++)
-            {
-                int score = new Node(new Vector(x,y)).getScore(end);
-                System.out.print("[" + score + "]");
-            }
-            System.out.println();
-        }
     }
     
     private static Node pathFind(Vector start, Vector end, boolean[][] walkableMap)
@@ -64,36 +70,36 @@ public class PathFinding {
         
         for(int i = 0; i < 100; i++)
         {
-            Node smallestF = smallestF(open, end);
+            Node smallestScore = smallestScore(open, end);
 
-            if(smallestF.getLocation().equals(end))
-                return smallestF;
+            if(smallestScore.getLocation().equals(end))
+                return smallestScore;
 
             for(Direction d : Direction.values())
             {
-                Node movement = new Node(smallestF, smallestF.getLocation().move(d));
+                Node movement = new Node(smallestScore, smallestScore.getLocation().move(d));
                 if(isWalkable(movement, walkableMap) && !open.contains(movement) && !closed.contains(movement))
                     open.add(movement);
             }
             
-            closed.add(smallestF);
-            open.remove(smallestF);
+            closed.add(smallestScore);
+            open.remove(smallestScore);
         }
         
         return null;
     }
     
-    private static Node smallestF(List<Node> adjacent, Vector start)
+    private static Node smallestScore(List<Node> adjacent, Vector start)
     {
-        int smallestF = Integer.MAX_VALUE;
+        int smallestScore = Integer.MAX_VALUE;
         Node smallest = null;
         for(Node n : adjacent)
         {
             int fValue = n.getScore(start);
-            if(fValue <= smallestF)
+            if(fValue < smallestScore)
             {
                 smallest = n;
-                smallestF = fValue;
+                smallestScore = fValue;
             }
         }
         
